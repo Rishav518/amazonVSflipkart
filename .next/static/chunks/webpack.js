eb809ca32ab14d9d99e9d9fe62bd15e047695f5d
rishav518 <rishav.arkay@gmail.com>
2024-03-01 01:16:52 +0530

    added files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a58ca16bbd9709bc"; }
+/******/ 		__webpack_require__.h = function() { return "bea9dbaf95fc6d83"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,11 @@
 import Link from 'next/link'
 import React from 'react'
 
-const Footer = () => {
+const Footer = () => { 
   return (
     <footer className='bg-blue-900 text-white flex items-center justify-center py-2'>
       <div className='max-w-7xl flex flex-col lg:flex-row items-center justify-around w-full'>
-        <div>Copyright &copy; 2024 | Arkay</div>
+        <div>Copyright &copy; 2024 | Arkrmay</div>
         <div className='flex items-center justify-between space-x-4'>
           <Link className='hover:text-fuchsia-500 ease-in-out duration-500' href='/about'>About US </Link>
           <Link className='hover:text-fuchsia-500 ease-in-out duration-500' href='/contact'>Contact US </Link>
